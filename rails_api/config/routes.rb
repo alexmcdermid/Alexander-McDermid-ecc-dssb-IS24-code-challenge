aca4e9a5b3f6741383d05224fa5a800ddf7b288a
Alex McDermid <alexander.mcdermid@alumni.ubc.ca>
2023-10-08 12:33:28 -0700

    build: rswag routing

--- rails_api/config/routes.rb
+++ rails_api/config/routes.rb
@@ -4,5 +4,7 @@ Rails.application.routes.draw do
   namespace :api do
     get '/health', to: 'application#health'
     resources :product, only: [:index, :show, :create, :update, :destroy]
+    mount Rswag::Ui::Engine => '/api-docs'
+    mount Rswag::Api::Engine => '/api-docs'
   end
 end
